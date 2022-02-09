13b5ab01e3cede5c2aaa0c0f72b870e9719044d6
tongZhezhe <9807671+tongzhezhe@user.noreply.gitee.com>
2022-02-10 00:22:39 +0800

    deploy

---
+++ precache-manifest.cb54a860b26fd637639280b163800114.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "359aad662b9b85a8c7a5",
+    "url": "/dog-money-website/css/app.9fc5de0b.css"
+  },
+  {
+    "revision": "9081f5ad2997c46da2b6",
+    "url": "/dog-money-website/css/chunk-4365fcda.29272d93.css"
+  },
+  {
+    "revision": "14050bb91aab543ba171",
+    "url": "/dog-money-website/css/chunk-55261e76.e744ffef.css"
+  },
+  {
+    "revision": "607d01c50c9832bc29d4",
+    "url": "/dog-money-website/css/chunk-fd70a29c.9f9a9993.css"
+  },
+  {
+    "revision": "a8d5e738be9199d9adb93c25ecf1a0a1",
+    "url": "/dog-money-website/img/logo.a8d5e738.png"
+  },
+  {
+    "revision": "f69597ef3e604147b23d8b89bdeba3dc",
+    "url": "/dog-money-website/img/qrcode.f69597ef.png"
+  },
+  {
+    "revision": "da4f7d6ae99d65706b08c75aa5aaf19c",
+    "url": "/dog-money-website/index.html"
+  },
+  {
+    "revision": "359aad662b9b85a8c7a5",
+    "url": "/dog-money-website/js/app.14667bc1.js"
+  },
+  {
+    "revision": "9081f5ad2997c46da2b6",
+    "url": "/dog-money-website/js/chunk-4365fcda.fcff16ca.js"
+  },
+  {
+    "revision": "14050bb91aab543ba171",
+    "url": "/dog-money-website/js/chunk-55261e76.bcf72c03.js"
+  },
+  {
+    "revision": "607d01c50c9832bc29d4",
+    "url": "/dog-money-website/js/chunk-fd70a29c.f2c4d9ff.js"
+  },
+  {
+    "revision": "a77b1c1766a42ae0c1f1",
+    "url": "/dog-money-website/js/chunk-vendors.d4d3c3bf.js"
+  },
+  {
+    "revision": "d79df65119b1aead3179d00caacde0b5",
+    "url": "/dog-money-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/dog-money-website/robots.txt"
+  }
+]);
